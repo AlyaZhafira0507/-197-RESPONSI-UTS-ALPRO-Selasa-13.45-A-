19d96d6e3348e8e575a078dc02ce179817095e1c
Alya0507 <alyazhafiraniha@gmail.com>
2025-04-15 14:52:14 +0700

    Add files via upload

---
+++ uANG.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float uangku;
+    cout << "Berapa Uangku = ";
+    cin >> uangku;
+    if (uangku >= 75000)
+       cout << "Sepatu Baru \n"; 
+}
+
+     
