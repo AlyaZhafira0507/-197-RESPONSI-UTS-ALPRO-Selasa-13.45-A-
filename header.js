09535e967078b452220b507051010b9d297cd88d
AlyaZhafira0507 <alyazhafira925@gmail.com>
2025-06-04 07:02:06 +0700

    Update header.js

--- header.js
+++ header.js
@@ -5,3 +5,5 @@ document.writeln("<h2>FAKULTAS TEKNOLOGI INDUSTRI</h2>");
 document.writeln("<h3>JURUSAN INFORMATIKA</h3>");
 document.writeln("<hr width='600' color='black'>");
 document.writeln("</center>");
+
+document.images["coolfan"].src = "gambar_on.gif";
